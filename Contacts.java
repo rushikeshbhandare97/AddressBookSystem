273c7e5c74683dee5fdce8679c7cc38a7e098569
technology-97 <rushikeshbhandare789@gmail.com>
2021-08-21 09:39:43 +0530

    Added:UC1_ContactsList    program is added

---
+++ Contacts.java
@@ -0,0 +1,28 @@
+public class Contact {
+
+
+    public void setFirstName(String firstName) {
+    }
+
+    public void setLastName(String lastName) {
+    }
+
+    public void setAddress(String address) {
+    }
+
+    public void setCity(String city) {
+    }
+
+    public void setState(String state) {
+    }
+
+    public void setPhoneNumber(double phoneNum) {
+    }
+}
+
+
+
+
+
+
+
